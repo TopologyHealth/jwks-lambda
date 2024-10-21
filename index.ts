12fd743a4c0240ad1cd6a2efb51a9a61fc28ae4d
Josh Liben <josh@topology.health>
2024-10-21 16:54:44 -0400

    convert key to pem JWKS

--- index.ts
+++ index.ts
@@ -1,33 +1,32 @@
+import { GetPublicKeyCommand, GetPublicKeyCommandOutput, KMSClient } from '@aws-sdk/client-kms';
+import assert from 'assert';
 import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
-import { GetPublicKeyCommand, GetPublicKeyCommandOutput, KMS, KMSClient } from '@aws-sdk/client-kms';
+import forge from 'node-forge';
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
-  // console.log(`Event: ${JSON.stringify(event, null, 2)}`);
-  // console.log(`Context: ${JSON.stringify(context, null, 2)}`);
-
-  const keyManagerClient = new KMSClient({region: 'us-east-2'})
+  const keyManagerClient = new KMSClient({ region: 'us-east-2' })
   const keyId = getKeyId(event)
-
   const publicKeyGetCommand = new GetPublicKeyCommand({ KeyId: keyId })
   const publicKeyCommandResult: GetPublicKeyCommandOutput = await keyManagerClient.send(publicKeyGetCommand)
+  const publicKeyAsDer = publicKeyCommandResult.PublicKey
+  assert(publicKeyAsDer, "A Der Encoded Public key must be present in the Keygetcommand result")
+  const pemPublicKey: string = derToPem(Buffer.from(publicKeyAsDer))
   const jwkSet = {
     keys: [
       {
         kty: "RSA",
         kid: keyId,
-        // n: publicKey.PublicKey.toString('base64'), // Encode modulus
-        e: 'AQAB' // Use a common exponent
+        n: Buffer.from(pemPublicKey).toString('base64'),
+        e: 'AQAB'
       }
     ]
   };
-
-  console.log(publicKeyCommandResult)
-
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'hello world',
-    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(jwkSet),
   };
 };
 
@@ -35,4 +34,21 @@ function getKeyId(event: APIGatewayEvent) {
   const resource = event.resource;
   const keyId = resource.replace('/', '')
   return keyId;
+}
+
+/**
+ * Converts DER-encoded public key to PEM format using node-forge.
+ * @param der Public key in DER format as a Buffer or Uint8Array.
+ * @returns PEM-encoded public key as a string.
+ */
+function derToPem(der: Buffer): string {
+  // Convert DER to Forge's Asn1 format
+  const asn1 = forge.asn1.fromDer(der.toString('binary'));
+
+  // Convert Asn1 to public key object
+  const publicKey = forge.pki.publicKeyFromAsn1(asn1);
+
+  // Convert public key to PEM format
+  const pem = forge.pki.publicKeyToPem(publicKey);
+  return pem;
 }
