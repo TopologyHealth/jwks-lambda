eff1d626c63ec4d91b575c12ea0cb31c04765576
Josh Liben <josh@topology.health>
2024-10-21 15:58:17 -0400

    first basic key getting test

--- index.ts
+++ index.ts
@@ -1,8 +1,28 @@
 import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import { GetPublicKeyCommand, GetPublicKeyCommandOutput, KMS, KMSClient } from '@aws-sdk/client-kms';
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
-  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
-  console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+  // console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+  // console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+
+  const keyManagerClient = new KMSClient({region: 'us-east-2'})
+  const keyId = getKeyId(event)
+
+  const publicKeyGetCommand = new GetPublicKeyCommand({ KeyId: keyId })
+  const publicKeyCommandResult: GetPublicKeyCommandOutput = await keyManagerClient.send(publicKeyGetCommand)
+  const jwkSet = {
+    keys: [
+      {
+        kty: "RSA",
+        kid: keyId,
+        // n: publicKey.PublicKey.toString('base64'), // Encode modulus
+        e: 'AQAB' // Use a common exponent
+      }
+    ]
+  };
+
+  console.log(publicKeyCommandResult)
+
   return {
     statusCode: 200,
     body: JSON.stringify({
@@ -10,3 +30,9 @@ export const handler = async (event: APIGatewayEvent, context: Context): Promise
     }),
   };
 };
+
+function getKeyId(event: APIGatewayEvent) {
+  const resource = event.resource;
+  const keyId = resource.replace('/', '')
+  return keyId;
+}
