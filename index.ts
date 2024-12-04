37df65b4bf1caed30dc44de8f86b064943b7ae77
Josh Liben <josh@topology.health>
2024-12-04 16:02:01 -0500

    added proxy handling

--- index.ts
+++ index.ts
@@ -5,15 +5,13 @@ import { JSONWebKeySet, JWK } from 'jose';
 import forge from 'node-forge';
 
 export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
-  const keyManagerClient = new KMSClient({ region: 'us-east-2' })
+  const region = process.env.API_ID ?? 'us-west-2';
+  const keyManagerClient = new KMSClient({ region: region })
   const keyId = getKeyId(event)
   const publicKeyGetCommand = new GetPublicKeyCommand({ KeyId: keyId })
   const publicKeyCommandResult: GetPublicKeyCommandOutput = await keyManagerClient.send(publicKeyGetCommand)
   const publicKeyAsDer = publicKeyCommandResult.PublicKey
   assert(publicKeyAsDer, "A Der Encoded Public key must be present in the Keygetcommand result")
-  // const pemPublicKey: string = derToPem(Buffer.from(publicKeyAsDer))
-  // const publicKey = await importSPKI(pemPublicKey, 'RS256', { extractable: true })
-  // const publicJwk = await exportJWK(publicKey)
 
   // Convert Uint8Array to Buffer
   const derBuffer = Buffer.from(publicKeyAsDer);
@@ -57,26 +55,11 @@ export const handler = async (event: APIGatewayEvent, context: Context): Promise
 
 function getKeyId(event: APIGatewayEvent) {
   const resource = event.resource;
+  const pathParams = event.pathParameters
   assert(resource, 'Resource must be present in the API gateway event object')
-  const keyId = resource.split('/').pop()
+  const proxy = pathParams ? pathParams['proxy'] : undefined
+  const keyId = proxy ?? resource.split('/').pop()
   return keyId;
-}
-
-/**
- * Converts DER-encoded public key to PEM format using node-forge.
- * @param der Public key in DER format as a Buffer or Uint8Array.
- * @returns PEM-encoded public key as a string.
- */
-function derToPem(der: Buffer): string {
-  // Convert DER to Forge's Asn1 format
-  const asn1 = forge.asn1.fromDer(der.toString('binary'));
-
-  // Convert Asn1 to public key object
-  const publicKey = forge.pki.publicKeyFromAsn1(asn1);
-
-  // Convert public key to PEM format
-  const pem = forge.pki.publicKeyToPem(publicKey);
-  return pem;
 }
 
 // Helper function to convert Uint8Array to Base64 URL encoding
